007048dad6131d2f8efa3fdf91e38f482c40c122
constdreamcoder <scdreamricher@gmail.com>
2024-03-10 16:30:52 +0900

    [CHORE] 탭바 순서 변경 - 북마크 > 검색 > 설정 순에서 검색 > 북마크 설정 순으로 수정

--- KoreaOneStep/Scenes/TabBarViewController.swift
+++ KoreaOneStep/Scenes/TabBarViewController.swift
@@ -11,24 +11,24 @@ final class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let mainVC = MainViewController()
         let bookmarkVC = BookmarkViewController()
         let settingVC = SettingViewController()
         
-        let mainNav = UINavigationController(rootViewController: mainVC)
         let bookmarkNav = UINavigationController(rootViewController: bookmarkVC)
+        let mainNav = UINavigationController(rootViewController: mainVC)
         let settingNav = UINavigationController(rootViewController: settingVC)
         
         mainNav.tabBarItem = UITabBarItem(
-            title: "북마크",
-            image: UIImage(systemName: "bookmark"),
-            selectedImage: UIImage(systemName: "bookmark.fill")
-        )
-        bookmarkNav.tabBarItem = UITabBarItem(
             title: "검색",
             image: UIImage(systemName: "magnifyingglass"),
             selectedImage: UIImage(systemName: "magnifyingglass")
+        )
+        bookmarkNav.tabBarItem = UITabBarItem(
+            title: "북마크",
+            image: UIImage(systemName: "bookmark"),
+            selectedImage: UIImage(systemName: "bookmark.fill")
         )
         settingNav.tabBarItem = UITabBarItem(
             title: "설정",
